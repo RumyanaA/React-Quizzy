d0cb1518f2742af7467b32638d714f1ce8f15467
Rumyana Atanasova <rumyana_atanasova@epam.com>
2022-05-11 11:01:53 +0300

    fixed debouncing bugs

--- client/src/app/shared-components/recipe-search-and-select/recipe-search-and-select.js
+++ client/src/app/shared-components/recipe-search-and-select/recipe-search-and-select.js
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useRef, useState } from "react";
 import InputField from "../../../shared/input/input-component";
 import { apiKey } from "../../../config/cooking-apiKey";
 import Spinner from "react-bootstrap/esm/Spinner";
@@ -9,47 +9,46 @@ const RecipeSearchAndSelect = () => {
   const [toggleDropDown, setToggleDropdown] = useState(false);
   const [recipeOptions, setRecipeOptions] = useState([]);
   const [selectedRecipe, setSelectedRecipe] = useState({});
+  const timeout = useRef();
   let url = `https://api.spoonacular.com/recipes/complexSearch?apiKey=${apiKey}&query=${searchValue}`;
-  function debounce(fn, delay) {
-    let timer;
-    return function () {
-      const context = this;
-      const args = arguments;
-      clearTimeout(timer);
-      timer = setTimeout(() => fn.apply(context, args), delay);
-      setLoadingRecipes(true);
-      setToggleDropdown(true);
-    };
-  }
-  const handleChange = debounce((value) => {
+  const handleChange = (value) => {
+    setToggleDropdown(true);
+    setLoadingRecipes(true);
     setSearchValue(value);
     if (!value) {
-        setRecipeOptions([])
-        setToggleDropdown(false);
-        return;
-            
+      setRecipeOptions([]);
+      setLoadingRecipes(false);
+      setToggleDropdown(false);
+      return;
     }
     url = `https://api.spoonacular.com/recipes/complexSearch?apiKey=${apiKey}&query=${value}`;
     fetchRecipes();
-  }, 1000);
-  const fetchRecipes = async () => {
-    await fetch(url)
-      .then((response) => response.json())
-      .then((data) => {
-        if (data.results.length > 0) {
-          setRecipeOptions(data.results);
-        }
-        setLoadingRecipes(false);
-      });
+  };
+  const fetchRecipes = () => {
+    clearTimeout(timeout.current);
+    timeout.current = setTimeout(async () => {
+      await fetch(url)
+        .then((response) => response.json())
+        .then((data) => {
+          if (data.results.length > 0) {
+            setRecipeOptions(data.results);
+          }
+          setLoadingRecipes(false);
+        });
+    }, 1000);
   };
   const handleSelectOption = (value) => {
+    setSearchValue("");
     setSelectedRecipe(value);
+    setRecipeOptions([]);
+    setToggleDropdown(false);
   };
   return (
     <>
       <InputField
         placeholder="Search recipes by keyword..."
         onChange={(e) => handleChange(e.target.value)}
+        value={searchValue}
       />
       <div>
         {toggleDropDown ? (
