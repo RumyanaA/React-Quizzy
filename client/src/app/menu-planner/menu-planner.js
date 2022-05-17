7c151d7306a5c6a84e9ece41c39a504051bc9f74
Anelia Radeva <anelia_radeva@epam.com>
2022-05-17 14:58:48 +0300

    Created pop-up

--- client/src/app/menu-planner/menu-planner.js
+++ client/src/app/menu-planner/menu-planner.js
@@ -9,18 +9,100 @@ import React, { useState } from "react";
 import PlannerModal from "./planner-modal/planner-modal";
 const MenuPlanner = () => {
   const [isOpen, setIsOpen] = useState(false);
+  const [dateToShow, setDateToShow] = useState(new Date());
   const [date, setDate] = useState(new Date());
+  const [menus, setMenus] = useState([{
+    date: '2022-05-07',
+    breakfast: { title: 'Bacon Caramels', details: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec con' },
+    lunch: {},
+    dinner: {}
+  },
+  {
+    date: '2022-05-02',
+    breakfast: {},
+    lunch: { title: 'Butternut Squash & Pear Soup: Real Convenience food', details: 'Recipe...' },
+    dinner: {}
+  }, {
+    date: '2022-06-01',
+    breakfast: {}
+    , lunch: {},
+    dinner: { title: 'Coconut Almond Cheesecake', details: 'Coconut almond cheesecake recipe' }
+  }]);
+  const [currMenu, setCurrMenu] = useState({});
+
+  const [events, setEvents] = useState([
+    {
+      
+      title: 'breakfast: Bacon Caramels',
+      date: '2022-05-07'
+    },
+    {
+      title: 'lunch: Butternut Squash & Pear Soup: Real Convenience food',
+      date: '2022-05-02'
+    },
+    {
+      title: 'dinner: Coconut Almond Cheesecake',
+      date: '2022-06-01'
+    }]);
   const editEvent = () => { };
   const handleDateClick = (info) => {
 
     setIsOpen(true);
-    const selectedDate = info.date.toString().slice(4,15);
-    setDate(selectedDate);
+    const selectedDateString = info.date.toString().slice(4, 15);
+    setDateToShow(selectedDateString);
 
-    // alert('Clicked on: ' + info.dateStr);
-    // // change the day's background color just for fun
-    // info.dayEl.style.backgroundColor = 'red';
+    const parsedDate = parseDate(selectedDateString);
+    setDate(parsedDate);
+
+
+    let m = menus.find(m => m.date === parsedDate);
+    if (!m) {
+      m = {
+        date: '',
+        breakfast: {},
+        lunch: {},
+        dinner: {}
+      }
+    }
+
+    setCurrMenu(m);
+
   };
+  const parseDate = (dateString) => {
+    const monthString = dateString.slice(0, 3).trim();
+    const day = dateString.slice(4, 6).trim();
+    const year = dateString.slice(7, 11).trim();
+
+    let month = '';
+    if (monthString === 'Jan') {
+      month = '01';
+    } else if (monthString === 'Feb') {
+      month = '02';
+    } else if (monthString === 'Mar') {
+      month = '03';
+    } else if (monthString === 'Apr') {
+      month = '04';
+    } else if (monthString === 'May') {
+      month = '05';
+    } else if (monthString === 'Jun') {
+      month = '06';
+    } else if (monthString === 'Jul') {
+      month = '07';
+    } else if (monthString === 'Aug') {
+      month = '08';
+    } else if (monthString === 'Sep') {
+      month = '09';
+    } else if (monthString === 'Oct') {
+      month = '10';
+    } else if (monthString === 'Nov') {
+      month = '11';
+    } else if (monthString === 'Dec') {
+      month = '12';
+    }
+
+    return `${year}-${month}-${day}`;
+
+  }
 
   return (
     <>
@@ -36,20 +118,14 @@ const MenuPlanner = () => {
             ]}
             initialView="dayGridMonth"
             dateClick={handleDateClick}
-            eventColor="blue"
+            eventColor="purple"
             eventTimeFormat={{
               hour: "numeric",
               minute: "2-digit",
               meridiem: "short",
             }}
-            // events={[...]}
-            events={[
-              { // this object will be "parsed" into an Event Object
-                title: 'The Title', // a property!
-                url: 'google.com'
-              },
-              { title: 'event 2', date: '2022-05-02' }
-            ]}
+
+            events={events}
             eventClick={editEvent}
             headerToolbar={{
               left: "prev,next today",
@@ -62,7 +138,10 @@ const MenuPlanner = () => {
           />
         </div>
       </div>
-      {isOpen ? <PlannerModal onClose={() => setIsOpen(false)} date={date} /> : null}
+      {isOpen ? <PlannerModal onClose={() => setIsOpen(false)} 
+      dateToShow={dateToShow} menu={currMenu} events={events} 
+      setEvents={setEvents}
+      date={date}/> : null}
     </>
   );
 };
--- client/src/app/menu-planner/planner-modal/planner-modal.js
+++ client/src/app/menu-planner/planner-modal/planner-modal.js
@@ -1,30 +1,71 @@
 import React, { useState } from "react";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faPen, faTrash, faCaretDown, faCaretUp } from '@fortawesome/free-solid-svg-icons';
+import { faPen, faTrash, faCaretDown, faCaretUp, faPlus } from '@fortawesome/free-solid-svg-icons';
 import "./planner-modal.scss";
 
-const PlannerModal = ({ date, onClose }) => {
+const PlannerModal = ({ dateToShow, onClose, menu,
+    events, setEvents, date }) => {
     const [isShowInput, setIsShowInput] = useState(false);
     const [indexInput, setIndexInput] = useState(0);
-
+    const [dailyMenu, setDailyMenu] = useState(menu);
     const [meal, setMeal] = useState([
-        { id: 1, text: 'Breakfast', isShow: false },
-        { id: 2, text: 'Lunch', isShow: false },
-        { id: 3, text: 'Dinner', isShow: false }
+        { id: 1, text: 'breakfast', isShow: false },
+        { id: 2, text: 'lunch', isShow: false },
+        { id: 3, text: 'dinner', isShow: false }
     ]);
+    function debounce(fn, delay) {
+        let timer;
+        return function () {
+            const context = this;
+            const args = arguments;
+            clearTimeout(timer);
+            timer = setTimeout(() => fn.apply(context, args), delay);
 
+        };
+    }
     const toggle = (index) => {
         meal[index].isShow = !meal[index].isShow
-        console.log(index)
         setMeal([...meal]);
     };
 
     const onEdit = (ind) => {
+
         if (indexInput === ind) {
             setIsShowInput(!isShowInput);
         } else {
             setIndexInput(ind);
         }
+    }
+    const onChange = debounce((value, mealItem) => {
+
+        //get recipe from api
+        let meal = menu[mealItem.text];
+        meal.title = value;
+        meal.details = "New details";
+        setDailyMenu(meal);
+
+        let selectedDayEvents = events.find(e => e.date === date && e.title.startsWith(mealItem.text));
+        let newTitle = `${mealItem.text}: ${value}`;
+
+        if (!selectedDayEvents) {
+            selectedDayEvents = {
+                title: '',
+                date: ''
+            }
+            selectedDayEvents.date = date;
+        }
+        selectedDayEvents.title = newTitle;
+        setEvents(prevEvents => [...prevEvents, selectedDayEvents]);
+    }, 1000);
+
+
+    const onDelete = (mealItem) => {
+        let meal = menu[mealItem.text];
+        meal.title = '';
+        meal.details = '';
+        setDailyMenu(meal);
+       
+    
     }
     return (
         <>
@@ -34,7 +75,7 @@ const PlannerModal = ({ date, onClose }) => {
                         <span className="close" onClick={onClose}>&times;</span>
                     </div>
                     <div className="modal-body">
-                        <h2 className="date">{date}</h2>
+                        <h2 className="date">{dateToShow}</h2>
                         <div>
                             {meal.map((mealItem, index) =>
                                 <div key={index} className="item">
@@ -42,24 +83,26 @@ const PlannerModal = ({ date, onClose }) => {
                                         <button className="arrow-buttons" >
                                             {mealItem.isShow ? <FontAwesomeIcon className="arrow-icon" icon={faCaretUp} /> :
                                                 <FontAwesomeIcon className="arrow-icon" icon={faCaretDown} />}
-                                            {mealItem.text}
+                                            {mealItem.text.charAt(0).toUpperCase() + mealItem.text.slice(1)}
                                         </button>
-                                        <h2>Title</h2>
+                                        {menu[mealItem.text].title &&
+                                            <h2 className="title" key={index}>{menu[mealItem.text].title}</h2>
+                                        }
                                     </div>
 
                                     {mealItem.isShow && <div className="recipe">
                                         <div className="input-buttons-container">
-                                            {isShowInput && (indexInput === index) ? <input id={index}></input> : null}
+                                            {isShowInput && (indexInput === index) ? <input onChange={(e) => onChange(e.target.value, mealItem)} id={index}></input> : null}
                                             <span className="buttons-container">
                                                 <button className="btn" onClick={() => onEdit(index)}>
-                                                    <FontAwesomeIcon icon={faPen} />   </button>
-                                                <button className="btn"><FontAwesomeIcon icon={faTrash} /></button>
+                                                    {isShowInput && (indexInput === index) ? <FontAwesomeIcon icon={faPlus} /> :
+                                                        <FontAwesomeIcon icon={faPen} />} </button>
+                                                <button className="btn" onClick={() => onDelete(mealItem)}>
+                                                    <FontAwesomeIcon icon={faTrash} /></button>
                                             </span>
                                         </div>
-                                        <p className="recipe-details">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec condimentum mi in massa mattis, eu sodales justo gravida. Sed eget sapien sodales, pretium turpis id, malesuada purus. Morbi molestie dui eget aliquet molestie. Integer pellentesque sit amet libero eget tristique. Ut sed leo non metus consequat egestas. Praesent eleifend diam et odio placerat ultricies. Nunc rutrum vehicula ex, a semper ante interdum cursus. Pellentesque vestibulum sapien a leo egestas faucibus.
-                                            Phasellus eu massa sem. Phasellus et placerat eros. Aenean in lorem eu orci condimentum aliquam. Mauris volutpat lorem efficitur leo tincidunt tempus. Suspendisse quam ex, blandit eu orci vitae, auctor semper nisi. Aenean laoreet scelerisque orci, et porta erat pellentesque ut. Sed semper et elit vel pellentesque. In vulputate eros nec volutpat scelerisque. Integer feugiat enim libero, ut luctus felis tristique sed.
-                                            Integer porttitor, elit ac blandit hendrerit, justo ante pellentesque erat, quis ullamcorper nisi ipsum a massa. Sed vitae arcu at leo congue venenatis. Aenean vel enim imperdiet urna egestas cursus. Proin et viverra velit, nec varius nibh. Mauris non aliquam quam, sit amet porta ante. Proin fermentum, mauris nec facilisis semper, quam purus venenatis nulla, sit amet laoreet arcu elit ut neque. Phasellus et scelerisque metus. Praesent ante erat, efficitur in elit vel, condimentum lobortis felis. Sed egestas dui id augue vulputate, sit amet vulputate odio luctus. Maecenas cursus dapibus dui eu vulputate. Curabitur egestas vitae elit a iaculis. In molestie volutpat egestas. Fusce massa sem, vulputate at est ut, aliquet auctor enim. Aenean accumsan quam erat, nec blandit libero lacinia eget. Phasellus vel eros et metus venenatis rutrum.
-                                            Sed tincidunt aliquam lorem quis luctus. Fusce rhoncus ut mi sed fermentum. In felis nunc, tempus eu eros sit amet, vulputate ultricies metus. Pellentesque in ante vitae sapien condimentum cursus. Fusce dolor orci, pellentesque a semper ullamcorper, molestie at ante. Fusce euismod enim at tristique mollis. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; In venenatis in libero sed fringilla. Morbi at nulla vitae mauris mollis finibus. Nunc diam ex, commodo a blandit id, tincidunt euismod nisi. Cras mattis justo auctor, scelerisque elit non, euismod erat. Vestibulum felis arcu, dictum eget egestas id, vestibulum id nisl.</p>
+                                        {menu[mealItem.text].title &&
+                                            <p className="recipe-details">{menu[mealItem.text].details}</p>}
                                     </div>}
                                 </div>)}
                         </div>
