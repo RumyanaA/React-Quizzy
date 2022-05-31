09871c0c2547bbb182c43715dc6322d5a3ede01c
Anelia Radeva <anelia_radeva@epam.com>
2022-05-31 15:50:24 +0300

    First test

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -1,11 +1,23 @@
 /* eslint-disable no-undef */
 import React from 'react';
+import { createMemoryHistory } from 'history';
 import { render, screen } from '@testing-library/react';
+import { Router } from 'react-router-dom';
 import Home from './app/home/home';
 
+const mockUser = { username: 'Anelia', password: '123' };
+window.localStorage.setItem('user', JSON.stringify(mockUser));
+
 test('renders Random food joke label', () => {
-  render(<Home />);
+  const history = createMemoryHistory();
+  render(
+    <Router location={history.location} navigator={history}>
+      <Home />
+    </Router>,
+
+  );
   const linkElement = screen.getByTestId('random-food-joke-label');
-  // expect(linkElement).toBeInTheDocument();
-  expect(linkElement).to.equal('Random food joke:');
+  expect(linkElement).toHaveTextContent(
+    'Random food joke:',
+  );
 });
--- client/src/index.js
+++ client/src/index.js
@@ -1,9 +1,12 @@
 import React from 'react';
-import { createRoot } from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import './index.scss';
+import { BrowserRouter } from 'react-router-dom';
 import AppRoutes from './AppRoutes';
 
-const rootElement = document.getElementById('root');
-
-const root = createRoot(rootElement);
-root.render(<AppRoutes />);
+ReactDOM.render(
+  <BrowserRouter>
+    <AppRoutes />
+  </BrowserRouter>,
+  document.getElementById('root'),
+);
