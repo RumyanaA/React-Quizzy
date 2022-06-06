755e59987a5eabbe21cd2b57e3decb02a29f2f84
Rumyana Atanasova <rumyana_atanasova@epam.com>
2022-06-06 18:13:06 +0300

    added more test to ingridients search component

--- client/src/app/discover/ingridient-search/__test__/ingridientSearch.test.js
+++ client/src/app/discover/ingridient-search/__test__/ingridientSearch.test.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-unused-expressions */
 /* eslint-disable no-unused-vars */
 /* eslint-disable prefer-const */
 /* eslint-disable no-undef */
@@ -33,6 +34,14 @@ function MockIngridientSearch() {
   );
 }
 
+const fetchIngridient = async (clickIngridient = true) => {
+  const inputEllement = screen.getByPlaceholderText(/Search ingridients.../i);
+  fireEvent.change(inputEllement, { target: { value: 'a' } });
+  const fetchedIngridient = await waitFor(() => screen.getByTestId('ingridient-testid-0'), { timeout: 1100 });
+  clickIngridient && fireEvent.click(fetchedIngridient);
+  return fetchedIngridient;
+};
+
 describe('Ingridients search tests', () => {
   test('should be able to type in input', async () => {
     render(
@@ -49,7 +58,7 @@ describe('Ingridients search tests', () => {
     );
     const inputEllement = screen.getByPlaceholderText(/Search ingridients.../i);
     fireEvent.change(inputEllement, { target: { value: 'banana' } });
-    const spinnerElement = screen.getByTestId('spinner');
+    const spinnerElement = screen.getByTestId('ingridients-spinner');
     expect(spinnerElement).toBeInTheDocument();
   });
 
@@ -59,7 +68,7 @@ describe('Ingridients search tests', () => {
     );
     const inputEllement = screen.getByPlaceholderText(/Search ingridients.../i);
     fireEvent.change(inputEllement, { target: { value: 'apple' } });
-    const spinnerElement = screen.getByTestId('spinner');
+    const spinnerElement = screen.getByTestId('ingridients-spinner');
     await waitFor(() => screen.getByTestId('ingridient-testid-0'), { timeout: 1100 });
     expect(spinnerElement).not.toBeInTheDocument();
   });
@@ -68,9 +77,75 @@ describe('Ingridients search tests', () => {
     render(
       <MockIngridientSearch />,
     );
-    const inputEllement = screen.getByPlaceholderText(/Search ingridients.../i);
-    fireEvent.change(inputEllement, { target: { value: 'a' } });
-    await waitFor(() => screen.getByTestId('ingridient-testid-0'), { timeout: 1100 });
-    expect(screen.getByTestId('ingridient-testid-0')).toBeInTheDocument();
+    const fetchedIngridient = await fetchIngridient(false);
+    expect(fetchedIngridient).toBeInTheDocument();
+  });
+
+  test('should display clicked ingridient in selected ingridients container', async () => {
+    render(
+      <MockIngridientSearch />,
+    );
+    const fetchedIngridient = await fetchIngridient();
+
+    const selectedIngridient = screen.getByTestId('selectedIngridient-testid-0');
+    expect(selectedIngridient).toHaveTextContent(fetchedIngridient.textContent);
+  });
+
+  test('should remove ingridient from selected ingridients when X-button is clicked', async () => {
+    render(
+      <MockIngridientSearch />,
+    );
+    await fetchIngridient();
+    const selectedIngridient = screen.getByTestId('selectedIngridient-testid-0');
+
+    const ingridientRemoveElement = screen.getByTestId('div-X-testid-0');
+    fireEvent.click(ingridientRemoveElement);
+    expect(selectedIngridient).not.toBeInTheDocument();
+  });
+
+  test('should show recipes-spinner while fetching recipes', async () => {
+    render(
+      <MockIngridientSearch />,
+    );
+    await fetchIngridient();
+    const searchRecipesButton = screen.getByRole('button');
+    fireEvent.click(searchRecipesButton);
+    const spinnerElement = screen.getByTestId('recipes-spinner');
+    expect(spinnerElement).toBeInTheDocument();
+  });
+
+  test('should fetch and render recipe after "find recipes" button is clicked', async () => {
+    render(
+      <MockIngridientSearch />,
+    );
+    await fetchIngridient();
+    const searchRecipesButton = screen.getByRole('button');
+    fireEvent.click(searchRecipesButton);
+    const fetchedRecipes = await waitFor(() => screen.getByTestId('recipe-testId-0'), { timeout: 1100 });
+    expect(fetchedRecipes).toBeInTheDocument();
+  });
+
+  test('should fetch and render 2 recipes after find recipes button is clicked', async () => {
+    render(
+      <MockIngridientSearch />,
+    );
+    await fetchIngridient();
+    const searchRecipesButton = screen.getByRole('button');
+    fireEvent.click(searchRecipesButton);
+    const fetchedRecipes = await waitFor(() => screen.getAllByTestId(/recipe-testId-/i), { timeout: 1100 });
+    expect(fetchedRecipes.length).toBe(2);
+  });
+
+  test('should not show recipes-spinner when fetching recipes has finished', async () => {
+    render(
+      <MockIngridientSearch />,
+    );
+    await fetchIngridient();
+    const searchRecipesButton = screen.getByRole('button');
+    fireEvent.click(searchRecipesButton);
+    const spinnerElement = screen.getByTestId('recipes-spinner');
+
+    await waitFor(() => screen.getAllByTestId(/recipe-testId-/i), { timeout: 1100 });
+    expect(spinnerElement).not.toBeInTheDocument();
   });
 });
--- client/src/mocks/handlers.js
+++ client/src/mocks/handlers.js
@@ -35,4 +35,21 @@ export const handlers = [
     );
   }),
 
+  rest.get('https://api.spoonacular.com/recipes/findByIngredients', (req, res, ctx) => {
+    req.url.searchParams.get('ingridients');
+    req.url.searchParams.get('apiKey');
+    return res(
+      ctx.status(200),
+      ctx.json([
+        {
+          id: 715412, title: 'Kiwi Popsicles - Summer Popsicle Series', image: 'https://spoonacular.com/recipeImages/715412-312x231.jpg', imageType: 'jpg', usedIngredientCount: [],
+        },
+        {
+          id: 644782, title: 'Gluten And Dairy Free Peanut Butter Cups', image: 'https://spoonacular.com/recipeImages/644782-312x231.jpg', imageType: 'jpg', usedIngredientCount: [],
+        },
+
+      ]),
+    );
+  }),
+
 ];
