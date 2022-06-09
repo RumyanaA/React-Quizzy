913a92f559d11d893b151a6fa9d02e9273cbd329
Bancho Petrov <bancho_petrov@epam.com>
2022-06-09 10:35:49 +0300

    RF-29 Refactored KeywordSearch - no debounce yet

--- client/src/app/discover/search/keyword/KeywordSearch.js
+++ client/src/app/discover/search/keyword/KeywordSearch.js
@@ -1,25 +1,36 @@
-import { React, useState } from 'react';
+import { React, useState, useEffect } from 'react';
 import Spinner from 'react-bootstrap/Spinner';
-import { useApi } from '../../../../hooks';
 import { InputField, SearchedRecipeCard, Title, NoDataFoundMessage } from '../../../../components';
-import { apiKey } from '../../../../config';
+import { fetchRecipesByKeyword, fetchRandomRecipes } from '../../../../service';
 import './keywordSearch.scss';
 
 function KeywordSearch() {
-  const [url, setUrl] = useState(`https://api.spoonacular.com/recipes/complexSearch?apiKey=${apiKey}&query=`);
+  const [keyword, setKeyword] = useState('');
 
-  const {
-    recipes,
-    hasResult,
-    loading,
-  } = useApi({ url });
+  const [recipes, setRecipes] = useState([]);
+
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    setLoading(true);
+    if (keyword) {
+      fetchRecipesByKeyword({ keyword })
+        .then((response) => response.json())
+        .then(({ results }) => setRecipes(results));
+    } else {
+      fetchRandomRecipes()
+        .then((response) => response.json())
+        .then(({ recipes: fetchedRecipes }) => setRecipes(fetchedRecipes));
+    }
+    setLoading(false);
+  }, [keyword]);
 
   return (
     <>
       <div className="search-container">
         <InputField
           placeholder="Search recipes by keyword..."
-          onChange={({ target: { value } }) => setUrl(`https://api.spoonacular.com/recipes/complexSearch?apiKey=${apiKey}&query=${value}`)}
+          onChange={({ target: { value } }) => setKeyword(value)}
         />
       </div>
       <Title title="Found Recipes" />
@@ -27,7 +38,7 @@ function KeywordSearch() {
         {loading && <Spinner animation="grow" variant="primary" />}
       </div>
       <div className="recipe-cards-container">
-        {hasResult
+        {recipes.length > 0
           ? recipes?.map((recipe, index) => <SearchedRecipeCard testId={`searched-recipe-card-${index}`} key={index} props={recipe} />)
           : <NoDataFoundMessage message="No Recipes Found" />}
       </div>
--- client/src/service.js
+++ client/src/service.js
@@ -2,10 +2,14 @@
 
 import { apiKey } from './config';
 
-export const fetchRandomRecipes = ({ number }) => fetch(
+export const fetchRandomRecipes = ({ number = 10 } = {}) => fetch(
   `https://api.spoonacular.com/recipes/random?apiKey=${apiKey}&number=${number}`,
 );
 
 export const fetchRandomFoodJoke = () => fetch(
   `https://api.spoonacular.com/food/jokes/random?apiKey=${apiKey}`,
+);
+
+export const fetchRecipesByKeyword = ({ keyword }) => fetch(
+  `https://api.spoonacular.com/recipes/complexSearch?apiKey=${apiKey}&query=${keyword}`,
 );
